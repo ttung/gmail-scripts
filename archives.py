6d738b649a928669cfe0cc00ec152b70b2d88501
Tony Tung <tonytung@merly.org>
2019-03-31 09:46:18 -0700

    label with conversation's labels

--- archives.py
+++ archives.py
@@ -14,86 +14,132 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import sys
-
 from oauth2client import client
 from gmail import *
+
 
 def main():
     Service.get()
 
     try:
-        vault_labelid = GmailLabel.get_id('vault')
-        personal_mail_labelid = GmailLabel.get_id('vault/personal-mail')
+        vault_label_id = GmailLabel.get_id('vault')
+        personal_mail_label_id = GmailLabel.get_id('vault/personal-mail')
 
-        def apply_personal_mail_label(messages):
+        def apply_vault_label(messages):
+            relabel_messages(
+                [message['id']
+                 for message in messages],
+                [],
+                [vault_label_id])
+
+        get_messages(
+            apply_vault_label,
+            labelIds=[],
+            q='has:nouserlabels !in:inbox !in:draft !in:snoozed !label:vault newer_than:45d'
+        )
+
+        def apply_related_label(messages):
             service = Service.get()
-            batch = service.new_batch_http_request()
 
-            apply_queue = []
-            def make_callback(message_id):
+            messages = {
+                message['id']: {'threadId': message['threadId']}
+                for message in messages
+            }
+            message_label_ops = []
+            personal_mail_messages_ids = []
+
+            def filter_labeled_messages_callback(message_id):
                 def callback(request_id, response, exception):
                     if exception:
                         raise exception
 
                     for labelid in response['labelIds']:
                         # ignore the vault label
-                        if labelid == vault_labelid:
+                        if labelid == vault_label_id:
                             continue
                         labelinfo = GmailLabel.get_label(labelid)
                         if labelinfo is None or labelinfo['type'] == 'user':
-                            break
-                    else:
-                        # include this in the apply queue.
-                        apply_queue.append((message_id, response))
+                            del messages[message_id]
 
                 return callback
 
-            for message in messages:
+            # filter the messages that have other labels besides vault.
+            batch = service.new_batch_http_request()
+            for message in messages.keys():
                 request = service.users().messages().get(
                     userId='me',
-                    id=message['id'],
+                    id=message,
                     format='minimal')
-                batch.add(request, callback=make_callback(message['id']))
-
+                batch.add(request, callback=filter_labeled_messages_callback(message))
             batch.execute()
 
-            if len(apply_queue) > 0:
+            def find_thread_label(thread_id):
+                def callback(request_id, response, exception):
+                    if exception:
+                        raise exception
+
+                    # gather all the labels to apply to messages in this thread.
+                    label_ids = set(
+                        label_id
+                        for message in response['messages']
+                        for label_id in message['labelIds']
+                    )
+
+                    # remove non-user labels
+                    label_ids.remove(vault_label_id)
+                    for label_id in label_ids.copy():
+                        label_info = GmailLabel.get_label(label_id)
+                        if label_info is not None or label_info['type'] != 'user':
+                            label_ids.remove(label_id)
+
+                    message_ids = [
+                        message_id
+                        for message_id, metadata in messages.items()
+                        if metadata['threadId'] == thread_id
+                    ]
+
+                    if len(label_ids) == 0:
+                        personal_mail_messages_ids.extend(message_ids)
+                    else:
+                        message_label_ops.append((message_ids, label_ids))
+
+                return callback
+
+            # filter the messages that have other labels besides vault.
+            batch = service.new_batch_http_request()
+            for thread_id in set(metadata['threadId'] for metadata in messages.values()):
+                request = service.users().threads().get(
+                    userId='me',
+                    id=thread_id,
+                    format='minimal')
+                batch.add(request, callback=find_thread_label(thread_id))
+            batch.execute()
+
+            for message_ids, label_ids in message_label_ops:
                 relabel_messages(
-                    [messageid
-                     for messageid, _ in apply_queue],
+                    message_ids,
                     [],
-                    [personal_mail_labelid])
-
-        get_messages(
-            apply_personal_mail_label,
-            q='label:vault !in:inbox !in:snoozed !label:vault/personal-mail !label:vault/fb')
-
-        def apply_both_labels(messages):
+                    label_ids)
             relabel_messages(
-                [message['id']
-                 for message in messages],
+                personal_mail_messages_ids,
                 [],
-                [vault_labelid])
+                [personal_mail_label_id])
 
         get_messages(
-            apply_both_labels,
-            labelIds=[],
-            q='(has:nouserlabels OR label:vault/fb) !in:inbox !in:draft !in:snoozed !label:vault newer_than:14d'
-        )
+            apply_related_label,
+            q='label:vault !in:inbox !in:snoozed !label:vault/personal-mail !label:vault/fb')
 
         def clear_label(messages):
             relabel_messages(
                 [message['id']
                  for message in messages],
-                [vault_labelid],
+                [vault_label_id],
                 [])
 
         get_messages(
             clear_label,
-            labelIds=[vault_labelid],
-            q='older_than:14d'
+            labelIds=[vault_label_id],
+            q='older_than:15d'
         )
 
     except client.AccessTokenRefreshError:
